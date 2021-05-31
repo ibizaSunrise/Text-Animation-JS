0db95c2ea98a08c98f1f1d9afc4072d0fcfea6f5
ibizaSunrise <milalemekh@gmail.com>
2021-05-31 20:42:01 +0100

    Add init function, upgrade method, animation loop

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ let mouse = {
 window.addEventListener('mousemove', function (e) {
     mouse.x = e.x;
     mouse.y = e.y;
-    console.log(mouse.x, mouse.y)
+    //console.log(mouse.x, mouse.y)
 });
 
 ctx.fillStyle = 'white';
@@ -23,7 +23,7 @@ ctx.fillText('A', 0, 30);
 const data = ctx.getImageData(0, 0, 100, 100);
 
 class Particle {
-    constructor(x, y){
+    constructor(x, y) {
         this.x = x;
         this.y = y;
         this.size = 3;
@@ -32,11 +32,39 @@ class Particle {
         this.density = (Math.random() * 30) + 1;
     }
 
-    draw(){
+    draw() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.closePath();
         ctx.fill();
     }
+
+    update() {
+        let dx = mouse.x - this.x;
+        let dy = mouse.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        distance < 300 ? this.size = 30 : this.size = 3;
+    }
 }
+
+function init() {
+    //particleArray = [];
+    for (let i = 0; i < 1000; i++) {
+        let x = Math.random() * canvas.width;
+        let y = Math.random() * canvas.height;
+        particleArray.push(new Particle(x, y))
+    }
+}
+init();
+
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    particleArray.forEach(item => {
+        item.draw();
+        item.update();
+
+    })
+    setTimeout(animate, 1000 / 10)
+}
+animate()
